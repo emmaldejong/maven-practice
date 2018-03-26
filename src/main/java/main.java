248a4bd766519ec5ea43ea29906c9c55f935ed74
emmadejong <dejong.l.emma@gmail.com>
2018-03-26 11:18:10 -0500

    added bonuses for extra practice

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -15,5 +15,18 @@ public class main {
         }
         System.out.println("Flipped case: \"" + StringUtils.swapCase(input) + "\"");
         System.out.println("Reversed string: \"" + StringUtils.reverse(input) + "\"");
+
+        System.out.println("Contains a vowel: " + containsVowel(input));
     }
+
+    public static boolean containsVowel(String input) {
+        if(StringUtils.containsAny(input, "aeiou")) {
+            return true;
+        }
+        return false;
+    }
+
+//    takes strng outpus boolean of contains no spaces
+//    takes string output string with all vowels removed
+//    capitalize if starts with consonant
 }
