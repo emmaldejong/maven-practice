b2b64ebca2c30665107277d91dc92c3cf3ff1e12
emmadejong <dejong.l.emma@gmail.com>
2018-03-26 11:35:56 -0500

    added more bonuses and notes

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,28 +5,39 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
+        // get user input, create and initialize scanner object
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Something: ");
         String input = scan.nextLine();
 
+        //solve the exercise problems to get the expected output
         System.out.println("You Entered: \"" + StringUtils.defaultString(input) + "\"");
-        if(!StringUtils.isNumeric(input)) {
-            System.out.println("\""+ input + "\" is not a number.");
+        if (!StringUtils.isNumeric(input)) {
+            System.out.println("\"" + input + "\" is not a number.");
         }
         System.out.println("Flipped case: \"" + StringUtils.swapCase(input) + "\"");
         System.out.println("Reversed string: \"" + StringUtils.reverse(input) + "\"");
 
+        //bonus outputs
         System.out.println("Contains a vowel: " + containsVowel(input));
+        System.out.println("Contains spaces: " + containsNoSpaces(input));
+        System.out.println(removeVowels(input));
     }
 
-    public static boolean containsVowel(String input) {
-        if(StringUtils.containsAny(input, "aeiou")) {
-            return true;
-        }
-        return false;
+    //create a method that takes in a string and outputs a boolean if the string contains a vowel
+    private static boolean containsVowel(String input) {
+        return StringUtils.containsAny(input, "aeiou");
     }
 
-//    takes strng outpus boolean of contains no spaces
-//    takes string output string with all vowels removed
-//    capitalize if starts with consonant
+    //    create a method that takes in a string and outputs a boolean if the string contains no spaces
+    private static boolean containsNoSpaces(String input) {
+        return StringUtils.containsWhitespace(input);
+    }
+
+    private static String removeVowels(String input) {
+        return StringUtils.remove(input, "aeiou");
+    }
 }
+//    create a method that takes in a string and outputs a string with all vowels removed if it has any
+//    create a method that takes in a string and outputs that same string and capitalizes it, if the string starts with a consonant
+
